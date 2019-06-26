578bc4d0b85b0e4cc8de317d1469526ad55b631b
xsu <xsu@thoughtworks.com>
2019-06-26 14:48:07 +0800

    add test code for external project A

--- src/test/java/ExpenseService/ExpenseServiceTest.java
+++ src/test/java/ExpenseService/ExpenseServiceTest.java
@@ -26,8 +26,13 @@ class ExpenseServiceTest {
     @Test
     void should_return_expense_type_A_if_project_is_external_and_name_is_project_A() throws UnexpectedProjectTypeException {
         // given
+        Project project = new Project(ProjectType.EXTERNAL, "Project A");
+
         // when
+        ExpenseType expenseType = ExpenseService.getExpenseCodeByProjectTypeAndName(project);
+
         // then
+        assertEquals(ExpenseType.EXPENSE_TYPE_A, expenseType);
     }
 
     @Test
